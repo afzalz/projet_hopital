462301e79b6a0e1f434935c6e8c9a91a622c4589
afzalz <afzalzoubeiri@gmail.com>
2016-05-16 21:31:52 +0200

    Add files via upload

---
+++ menu.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8" />
+  <title>index</title>
+  <!-- Bootstrap -->
+  <link href="css/bootstrap.min.css" rel="stylesheet" media="screen">
+</head>
+
+
+<body>
+  <nav class="navbar navbar-inverse">
+    <div class="container-fluid">
+      <!-- Brand and toggle get grouped for better mobile display -->
+      
+
+      <!-- Collect the nav links, forms, and other content for toggling -->
+      <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+        <ul class="nav navbar-nav">
+          <li class="active"><a href="index.php">Accueil <span class="sr-only">(current)</span></a></li>
+          <li><a href="medecin.php">Medecin</a></li>
+          <li><a href="client.php">Client</a></li>
+          <li><a href="Recherche_client.php">Recherche</a></li>
+          <li><a href="inscription_clients.php">Inscription client</a></li>
+        </ul>
+        <ul class="nav navbar-nav navbar-right">
+          <li><a href="inscription.php">Inscription</a></li>
+          <li><a href="login.php">Connexion</a></li>
+        </ul>
+      </div><!-- /.navbar-collapse -->
+    </div><!-- /.container-fluid -->
+  </nav>
+
+
+
+
+
+
+</body>
+</html>
