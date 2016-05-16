61db6b1ab3b6fb2b09c277d62f255fdf87e1e606
afzalz <afzalzoubeiri@gmail.com>
2016-05-16 21:29:50 +0200

    Add files via upload

---
+++ profil_infirmier.php
@@ -0,0 +1,116 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8" />
+  <title>Medecin</title>
+  <!-- Bootstrap -->
+  <link href="css/bootstrap.css" rel="stylesheet" media="screen">
+  <?php include 'menu.php'; ?>
+</head>
+
+<body>
+
+<div class="container-fluid">
+  <div class="row">
+      <div class=".col-xs-12 .col-md-9 sidebar">
+        <ul class="nav nav-sidebar">
+          <li class="active"><a href="#">Modifier Profile<span class="sr-only">(current)</span></a></li>
+          <li><a href="#">Reports</a></li>
+          <li><a href="#">Analytics</a></li>
+          <li><a href="#">Export</a></li>
+        </ul>
+        <ul class="nav nav-sidebar">
+          <li><a href="">Nav item</a></li>
+          <li><a href="">Nav item again</a></li>
+          <li><a href="">One more nav</a></li>
+          <li><a href="">Another nav item</a></li>
+          <li><a href="">More navigation</a></li>
+        </ul>
+        <ul class="nav nav-sidebar">
+          <li><a href="">Nav item again</a></li>
+          <li><a href="">One more nav</a></li>
+          <li><a href="">Another nav item</a></li>
+        </ul>
+      </div>
+
+
+
+      <div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
+        <h2 class="sub-header">Emploi du temps</h2>
+        <div class="table-responsive">
+         
+          <table class="table table-striped">
+            <thead>
+          <tr>
+              <th>Jour</th>
+              <th>Heure debut</th>
+              <th>Heure fin</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Lundi</td>
+              <td>8h30</td>
+              <td>17h00</td>
+            </tr>
+            <tr>
+              <td>Mardi</td>
+              <td>8h30</td>
+              <td>17h00</td>
+            </tr>
+            <tr>
+              <td>Mercredi</td>
+              <td>9h00</td>
+              <td>17h30</td>
+            </tr>
+            <tr>
+              <td>Jeudi</td>
+              <td>9h00</td>
+              <td>17h30</td>
+            </tr>
+            <tr>
+              <td>Vendredi</td>
+              <td>9h00</td>
+              <td>17h30</td>
+            </tr>
+            </tbody>
+          </table>
+
+
+        </div>
+      </div>
+      </div>
+      </div>
+
+      <div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
+        <h2 class="sub-header">Service</h2>
+        <div class="table-responsive">
+         
+          <table class="table table-striped">
+            <thead>
+          <tr>
+              <th>Service</th>
+              <th>Batiment</th>
+              <th>Etage</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Cardio</td>
+              <td>Batiment C</td>
+              <td>Etage 5</td>
+            </tr>
+            </tbody>
+          </table>
+
+
+        </div>
+      </div>
+      </div>
+      </div>
+
+
+
+
+    </body>
+    </html>
